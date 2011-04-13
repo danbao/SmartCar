6d46031076386446030ea3c7656932f9e4172db8
lz1989 <lz1989@f928a65b-cbae-257c-6b38-8991fd97a9c5>
2011-04-13 05:35:23 +0000

    Wireless迁移到float下了，可以实现了

---
+++ trunk/test/Wireless2/Sources/main.h
@@ -0,0 +1,26 @@
+/*全局变量都定义在这里，方便整理*/
+float Prop1,Prop2,Diff1,Diff2;
+int SpeedMax,SpeedMin;
+char lcd[100];
+char SCIreceive[100];
+//===============时钟初始化========================//
+void SetBusCLK_40M()
+{   
+    CLKSEL=0X00;				//disengage PLL to system
+    PLLCTL_PLLON=1;			//turn on PLL
+    SYNR =0xc0 | 0x04;                        
+    REFDV=0x80 | 0x01; 
+    POSTDIV=0x00;       //pllclock=2*osc*(1+SYNR)/(1+REFDV)=80MHz;
+    _asm(nop);          //BUS CLOCK=40M
+    _asm(nop);
+    while(!(CRGFLG_LOCK==1));	  //when pll is steady ,then use it;
+    CLKSEL_PLLSEL =1;		        //engage PLL to system; 
+}   
+//==============延时程序================================//
+void delayms(int ms)    //延时程序。
+{   
+   int ii,jj;
+   if (ms<1) ms=1;
+   for(ii=0;ii<ms;ii++)
+     for(jj=0;jj<3338;jj++);    //40MHz--1ms      
+}
