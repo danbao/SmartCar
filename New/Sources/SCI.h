77416cbf3e9493c6fc628d13989943ebd367ab56
Chen.Ryder@gmail.com <ryderkun@users.noreply.github.com>
2011-05-25 13:41:33 +0000

    pit

--- New/Sources/SCI.h
+++ New/Sources/SCI.h
@@ -5,6 +5,7 @@
 ----------------------------------------------*/
 char jiguang[3],hongwai[28];
 int sdj,xdj,speed;
+byte light_laser_array[12]="011000110010";
 /*--------------------------------------------
 SCI_RXD: 串口接收函数
 编写日期：20110411
@@ -65,7 +66,7 @@ static void SCI_Init(void)  //SCI
 激光状态转换为16进制
 编写日期：200110521
 -----------------------------------------  */         
-void Testjiguang(byte a[12]) 
+void Testjiguang(byte a[]) 
 { 
 char b[3]; 
 int i,result = 0,k = 1,j = 0; 
@@ -114,9 +115,9 @@ void TestSMinfo(){
   //  sdj=rand()%10000;
  //   xdj=rand()%10000;
 //	speed=rand()%10000;
-	Testjiguang(light_temp_laser_array);
+	Testjiguang(light_laser_array);
 //	Testhongwai(IR_temp_laser_array);
   Clear_baitou();
-    sprintf(SCIreceive,"SED%.s%d",jiguang,JG_clear_position);
+    sprintf(SCIreceive,"SED%.3s%4d",jiguang,JG_clear_position);
     SCISend_chars(SCIreceive);
     }
