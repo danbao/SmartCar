00d4c74dafd039b9e36abd0eb81369f2dc3f866e
danbao <danbao@users.noreply.github.com>
2011-07-08 14:23:50 +0000

    LCD写完了

--- test/LCD/Sources/main.c
+++ test/LCD/Sources/main.c
@@ -8,22 +8,25 @@
 #define LASER_MAX 12          //激光管数量
 byte light_temp_laser_array[12]={1,0,1,0,1,0,1,1,1,0,0,0};
 int IR_temp_laser_array[7]={130,0,200,0,255,100,150};
+int xiugaizhi;
 #include "LCD.h"
 
 void main(void)
 {
-   uchar x;	
+   uchar key;	
  // EnableInterrupts;
 	LCD_init();  //初始化LCD模块 
-//LCD_start();
-
+  //LCD_start();
+  // delay_nms(5000);
+   LCD_clear();
+  LCD_show();
   for(;;)
   	{ 
   	if (LCD_checkkey()==0x00) continue;
 	   	else 
-		{     LCD_clear();
-          x= LCD_keyscan();
-          LCD_write_zi(0,0,x);
+		{
+          key= LCD_keyscan();
+          LCD_determine(LCD_keytran(key));
     delay_nms(100);
 		}
   	}
